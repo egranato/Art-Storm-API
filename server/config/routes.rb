0966a4a629a716f2606b58ec3da794ed9bcc0efc
Anthony <anthonyguitarman13@gmail.com>
2017-03-12 20:48:04 -0600

    my branch

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :artist do
+
+  end
+
+  resources :user do
+    
+  end
+
 end
