f3b79ee8e5dd9722858d822fa738d323dfe51500
Anthony <anthonyguitarman13@gmail.com>
2017-03-14 15:14:13 -0600

    routes

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  jsonapi_resources :users
-  jsonapi_resources :artists
-  jsonapi_resources :works
+  jsonapi_resources :user
+  jsonapi_resources :artist
+  jsonapi_resources :work
   jsonapi_resources :follow
 end
