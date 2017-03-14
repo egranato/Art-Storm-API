70eb217f9724c609414ed4e4851f3ef558d67736
Ethan Granato <ethan@granato.net>
2017-03-14 15:11:21 -0600

    fucking seeded hoes

--- server/app/resource/work_resource.rb
+++ server/app/resource/work_resource.rb
@@ -1,6 +1,4 @@
 class WorkResource < JSONAPI::Resource
-
-  attributes :name, :work_type, :description, :visual_url, :artist_id
-
+  attributes :name, :work_type, :description, :visual_url, :artists_id
   belongs_to :artists
 end
