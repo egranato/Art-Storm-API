a78efc52a89d7eb2a740bb2937410b7ffb3b3171
Anthony <anthonyguitarman13@gmail.com>
2017-03-14 18:24:30 -0600

    working on controllers & added rack-cors

--- server/app/controllers/user_controller.rb
+++ server/app/controllers/user_controller.rb
@@ -1,3 +1,31 @@
 class UserController < JSONAPI::ResourceController
+ def index
+   users = User.all
+   render json: {status: 'Success', message: 'Loaded all users', data: users}, status: :ok
+ end
+
+ def show
+   user = User.find(user_path[:id])
+   render json: user
+ end
+
+ def create
+   user = User.new(user_path[:id])
+
+     render json: {status: 'Created', data: user}, status: :ok
+ end
+ #
+ #
+ # private
+ #
+ # def set_user
+ #   user = User.find(user_path[:id])
+ # end
+
+ # def user_path
+ #   path.require(:user).permit(:username, :email, :first, :last, :password)
+ # end
+
+
 
 end
